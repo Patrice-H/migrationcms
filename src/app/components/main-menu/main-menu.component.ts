6d6666eaca5184d53b86d0243b1b22a85501f3a6
Patrice-H <webmestre64@gmail.com>
2023-08-15 16:43:01 +0200

    Implements main menu

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss'],
 })
-export class MainMenuComponent {}
+export class MainMenuComponent implements OnInit {
+  isOpened!: boolean;
+  chevronState!: string;
+
+  ngOnInit(): void {
+    this.isOpened = false;
+    this.chevronState = 'down';
+    this.resetMenu();
+  }
+
+  openCloseMenu(): void {
+    this.isOpened = !this.isOpened;
+    this.chevronState = this.isOpened ? 'up' : 'down';
+    this.resetMenu();
+  }
+
+  resetMenu(): void {
+    const chevron = document.getElementById('chevron');
+    chevron?.setAttribute('class', '');
+    chevron?.classList.add(this.chevronState);
+  }
+}
