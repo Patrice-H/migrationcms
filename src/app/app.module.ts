5faa5285a7d3ce8d55e2332e3b4360bd5145b80e
Patrice-H <webmestre64@gmail.com>
2023-08-16 14:35:07 +0200

    Update component declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MainMenuModule } from './components/main-menu/main-menu.module';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { DocumentationComponent } from './pages/documentation/documentation.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ServicesComponent,
+    DocumentationComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, MainMenuModule],
   providers: [],
   bootstrap: [AppComponent],
