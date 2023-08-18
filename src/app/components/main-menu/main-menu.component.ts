e2a3545c01d33870b7ef82f08cbcbee4ac61206a
Patrice-H <webmestre64@gmail.com>
2023-08-18 14:16:41 +0200

    Update menu

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-menu',
@@ -8,11 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class MainMenuComponent implements OnInit {
   isOpened!: boolean;
   chevronState!: string;
+  @Input() activeTab = '';
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.isOpened = false;
     this.chevronState = 'down';
+    const path =
+      this.router.url.split('/')[1] === ''
+        ? 'home'
+        : this.router.url.split('/')[1];
+    this.activeTab = path;
+    const domTab = document.getElementById(this.activeTab);
+    domTab?.classList.add('active');
     this.resetMenu();
+  }
+
+  updateActiveTab(submenu: boolean, tab: string = '#') {
+    tab === '#' ? null : (this.activeTab = tab);
+    submenu ? this.openCloseMenu() : null;
+    this.updateDOM();
+  }
+
+  updateDOM() {
+    const mainMenu = Array.from(document.getElementsByTagName('a'));
+    mainMenu.forEach((tab) => tab.classList.remove('active'));
+    const domTab = document.getElementById(this.activeTab);
+    console.log(domTab);
+    domTab?.classList.add('active');
   }
 
   openCloseMenu(): void {
