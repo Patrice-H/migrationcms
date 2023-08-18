58bb899d7aa6743ae2753c7c64cffca093844977
Patrice-H <webmestre64@gmail.com>
2023-08-19 00:33:12 +0200

    Updates routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { DocumentationComponent } from './pages/documentation/documentation.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ForumComponent } from './pages/forum/forum.component';
+import { MessagingComponent } from './pages/messaging/messaging.component';
+import { AccountComponent } from './pages/account/account.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { SecurityComponent } from './pages/security/security.component';
+import { TermOfUseComponent } from './pages/term-of-use/term-of-use.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,6 +18,12 @@ const routes: Routes = [
   { path: 'services', component: ServicesComponent },
   { path: 'docs', component: DocumentationComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'forum', component: ForumComponent },
+  { path: 'messaging', component: MessagingComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'privacy', component: PrivacyComponent },
+  { path: 'security', component: SecurityComponent },
+  { path: 'term-of-use', component: TermOfUseComponent },
 ];
 
 @NgModule({
