6e8d271c508855b2ab90953d2c8c5b8fda2bf7bb
Patrice-H <webmestre64@gmail.com>
2023-08-19 00:36:45 +0200

    Create blank login/signup interface

---
+++ src/app/components/log-sign/log-sign.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-sign',
+  templateUrl: './log-sign.component.html',
+  styleUrls: ['./log-sign.component.scss']
+})
+export class LogSignComponent {
+
+}
