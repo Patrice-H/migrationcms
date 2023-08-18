344d1cd1dd792679509d534ffb6e55cbcb1ab4cd
Patrice-H <webmestre64@gmail.com>
2023-08-19 00:30:05 +0200

    Create blank security page

---
+++ src/app/pages/security/security.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security',
+  templateUrl: './security.component.html',
+  styleUrls: ['./security.component.scss'],
+})
+export class SecurityComponent {}
