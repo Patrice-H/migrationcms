91e307f93c4e3409f4ee08ef96f82f36fdad20bf
Patrice-H <webmestre64@gmail.com>
2023-08-22 13:38:50 +0200

    Adds Integrity component tests

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 import { MainMenuComponent } from '../main-menu/main-menu.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -17,7 +18,21 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
+  /*** Component Integrity Tests ***/
+
   it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render logo img', () => {
+    const { debugElement } = fixture;
+    const logo = debugElement.query(By.css('img'));
+    expect(logo).toBeTruthy();
+    expect(logo.attributes['src']?.includes('logo1.3.0.jpg')).toBe(true);
+  });
+
+  it('should render the navbar', () => {
+    const { debugElement } = fixture;
+    expect(debugElement.query(By.css('nav'))).toBeTruthy();
   });
 });
