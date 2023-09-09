6be802f28341747d488830af4efd3928a3e20d1f
Patrice-H <webmestre64@gmail.com>
2023-09-09 23:38:05 +0200

    Updates component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  @Input() activePage = '';
+export class HeaderComponent implements OnInit {
+  @Input() activePage;
+  isConnected!: boolean;
+
+  constructor(private router: Router) {
+    const path =
+      this.router.url.split('/')[1] === ''
+        ? 'home'
+        : this.router.url.split('/')[1];
+    this.activePage = path;
+  }
+
+  ngOnInit(): void {
+    if (
+      this.activePage === 'home' ||
+      this.activePage === 'docs' ||
+      this.activePage === 'contact'
+    ) {
+      this.isConnected = true;
+    } else {
+      this.isConnected = false;
+    }
+    // + Session storage logic
+  }
 }
