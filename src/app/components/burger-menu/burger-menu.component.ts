044b9f52addf2b0e119884cfbff53330d29b0c05
Patrice-H <webmestre64@gmail.com>
2023-09-11 22:11:05 +0200

    Connect burger menu

--- src/app/components/burger-menu/burger-menu.component.ts
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-burger-menu',
   templateUrl: './burger-menu.component.html',
-  styleUrls: ['./burger-menu.component.scss']
+  styleUrls: ['./burger-menu.component.scss'],
 })
 export class BurgerMenuComponent {
+  @Output() burgerLink = new EventEmitter<string>();
 
+  navigate(event: MouseEvent, target: string) {
+    if (
+      target === 'services' ||
+      target === 'account' ||
+      target === 'messaging' ||
+      target === 'forum'
+    ) {
+      event.preventDefault();
+      this.burgerLink.emit(target);
+    }
+  }
 }
