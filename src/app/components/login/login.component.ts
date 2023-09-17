1374cb81c50bf8ae5d6cf8393e5679f2f1a09a78
Patrice-H <webmestre64@gmail.com>
2023-09-18 00:20:52 +0200

    Creates login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
+import { FormService } from 'src/app/services/form.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  isHidden: boolean = false;
+  errorMessage: string = '';
+  userRemembered: boolean = false;
+  password: string = '';
+  @Output() closeApp = new EventEmitter<boolean>();
+  @Output() nextStep = new EventEmitter<string>();
 
+  constructor(
+    private storage: StorageService,
+    private formElement: FormService
+  ) {}
+
+  cancel(op: boolean): void {
+    this.isHidden = op;
+    this.closeApp.emit(true);
+  }
+  reset(): void {
+    this.formElement.resetComponent('pass');
+    this.errorMessage = '';
+  }
+  modify(value: string) {
+    this.password = value;
+  }
+  remember() {
+    const checkbox = document.getElementById('remembered');
+    checkbox?.classList.remove('error');
+    this.userRemembered = !this.userRemembered;
+    if (this.userRemembered) {
+      checkbox?.classList.add('approved');
+      this.storage.setExpiryDate();
+    } else {
+      checkbox?.classList.remove('approved');
+    }
+  }
+  dataEntry(): boolean {
+    if (this.password === '') {
+      return false;
+    }
+
+    return true;
+  }
+  controlForm(event: MouseEvent) {
+    let errors = false;
+    event.preventDefault();
+    const input = document.getElementById('pass');
+    const message = document.getElementById('error-message');
+
+    if (!this.dataEntry()) {
+      input?.classList.add('error');
+      this.errorMessage = 'Votre Mot de passe est requis';
+      message?.classList.remove('hidden');
+      errors = true;
+    }
+    if (!errors) {
+      this.nextStep.emit('signUp');
+    }
+  }
 }
