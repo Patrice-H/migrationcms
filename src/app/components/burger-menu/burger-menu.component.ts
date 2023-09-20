72126414c759c458f56647003945bb34a0e71cb6
Patrice-H <webmestre64@gmail.com>
2023-09-20 11:12:22 +0200

    Implements dynamic connexion button

--- src/app/components/burger-menu/burger-menu.component.ts
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -1,22 +1,35 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-burger-menu',
   templateUrl: './burger-menu.component.html',
   styleUrls: ['./burger-menu.component.scss'],
 })
-export class BurgerMenuComponent {
+export class BurgerMenuComponent implements OnInit {
+  buttonLabel!: string;
   @Output() burgerLink = new EventEmitter<string>();
 
+  constructor(private storage: StorageService) {}
+
+  ngOnInit(): void {
+    this.buttonLabel = this.storage.isConnected() ? 'Déconnexion' : 'Connexion';
+  }
+
   navigate(event: MouseEvent, target: string) {
-    if (
-      target === 'services' ||
-      target === 'account' ||
-      target === 'messaging' ||
-      target === 'forum'
-    ) {
-      event.preventDefault();
-      this.burgerLink.emit(target);
+    event.preventDefault();
+    this.burgerLink.emit(target);
+  }
+  logInOut(event: MouseEvent) {
+    event.preventDefault();
+    if (this.buttonLabel === 'Connexion') {
+      this.storage.writeToken();
+      this.buttonLabel = 'Déconnexion';
+    } else {
+      this.storage.disconnect();
+      this.buttonLabel = 'Connexion';
     }
+
+    this.burgerLink.emit('account');
   }
 }
--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-main-menu',
@@ -9,10 +10,11 @@ import { Router } from '@angular/router';
 export class MainMenuComponent implements OnInit {
   isOpened!: boolean;
   chevronState!: string;
+  buttonLabel!: string;
   @Input() activeTab = '';
   @Output() mainLink = new EventEmitter<string>();
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private storage: StorageService) {}
 
   ngOnInit(): void {
     this.isOpened = false;
@@ -24,20 +26,14 @@ export class MainMenuComponent implements OnInit {
     this.activeTab = path;
     const activeTab = document.getElementById(this.activeTab);
     activeTab?.classList.add('active');
+    this.buttonLabel = this.storage.isConnected() ? 'Déconnexion' : 'Connexion';
     this.resetMenu();
     this.updateSubMenu();
   }
 
   navigate(event: MouseEvent, target: string) {
-    if (
-      target === 'services' ||
-      target === 'account' ||
-      target === 'messaging' ||
-      target === 'forum'
-    ) {
-      event.preventDefault();
-      this.mainLink.emit(target);
-    }
+    event.preventDefault();
+    this.mainLink.emit(target);
   }
 
   updateSubMenu() {
@@ -58,5 +54,16 @@ export class MainMenuComponent implements OnInit {
     const chevron = document.getElementById('chevron');
     chevron?.setAttribute('class', '');
     chevron?.classList.add(this.chevronState);
+  }
+  logInOut(event: MouseEvent) {
+    event.preventDefault();
+    if (this.buttonLabel === 'Connexion') {
+      this.storage.writeToken();
+      this.buttonLabel = 'Déconnexion';
+    } else {
+      this.storage.disconnect();
+      this.buttonLabel = 'Connexion';
+    }
+    this.mainLink.emit('account');
   }
 }
