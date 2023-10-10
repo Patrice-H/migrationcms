35c1642804226c3319ff2cd37cb80d4082bcd820
Patrice-H <webmestre64@gmail.com>
2023-10-10 09:25:18 +0200

    Updates tests

--- src/app/components/signup/signup.component.spec.ts
+++ src/app/components/signup/signup.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupComponent } from './signup.component';
+import { BlackWindowComponent } from '../black-window/black-window.component';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -8,7 +9,7 @@ describe('SignupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SignupComponent],
+      declarations: [SignupComponent, BlackWindowComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignupComponent);
