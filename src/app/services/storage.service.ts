76b16df5605bbfb8b3389db951cff14a9d3a40e8
Patrice-H <webmestre64@gmail.com>
2023-10-10 09:22:43 +0200

    Updates functionalities

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -16,9 +16,8 @@ export class StorageService {
     );
     if (localStorage.getItem('user-preferences') === null) {
       const userPref = {
-        token:
-          '6hrFDATxrG9w14QY9wwnmVhLE0Wg6LIvwOwUaxz761m1JfRp4rs8Mzozk5xhSkw0_MQz6bpcJnrFUDwp5lPPFC157dHxbkKlDiQ9XY3ZIP8zAGCsS8ruN2uKjIaIargX',
-        expires: expiresDate,
+        token: '',
+        expires: '',
         userName: '',
         eMail: '',
         lastConnexion: connexionDate,
@@ -52,9 +51,35 @@ export class StorageService {
     const rawData = this.getLocalStorage();
     const now = new Date();
     const delay = now.getDate() + 1;
-    const expireDate = now.setDate(delay);
+    const expireDate = new Date(now.setDate(delay));
     let storage = rawData === null ? null : JSON.parse(rawData);
     storage.expires = expireDate;
+    localStorage.setItem('user-preferences', JSON.stringify(storage));
+  }
+
+  isConnected(): boolean {
+    const rawData = this.getLocalStorage();
+    let storage = rawData === null ? null : JSON.parse(rawData);
+    const now = new Date();
+    const expiresDate = storage === null ? now : new Date(storage.expires);
+    if (expiresDate > now && storage.token !== '') {
+      return true;
+    }
+
+    return false;
+  }
+  writeToken() {
+    const rawData = this.getLocalStorage();
+    let storage = rawData === null ? null : JSON.parse(rawData);
+    storage.token =
+      '6hrFDATxrG9w14QY9wwnmVhLE0Wg6LIvwOwUaxz761m1JfRp4rs8Mzozk5xhSkw0_MQz6bpcJnrFUDwp5lPPFC157dHxbkKlDiQ9XY3ZIP8zAGCsS8ruN2uKjIaIargX';
+    localStorage.setItem('user-preferences', JSON.stringify(storage));
+  }
+  disconnect() {
+    const rawData = this.getLocalStorage();
+    let storage = rawData === null ? null : JSON.parse(rawData);
+    storage.token = '';
+    storage.expires = '';
     localStorage.setItem('user-preferences', JSON.stringify(storage));
   }
 }
