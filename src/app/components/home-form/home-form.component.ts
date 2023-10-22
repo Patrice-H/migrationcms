2d7a5cf9f829d57455d0814baf7b9181c189e78c
Patrice-H <webmestre64@gmail.com>
2023-10-22 12:09:51 +0200

    Updates component

--- src/app/components/home-form/home-form.component.ts
+++ src/app/components/home-form/home-form.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-form',
   templateUrl: './home-form.component.html',
   styleUrls: ['./home-form.component.scss'],
 })
-export class HomeFormComponent {
-  setCmsSource(): string {
-    return 'prestashop';
+export class HomeFormComponent implements OnInit {
+  panelHidden!: boolean;
+  panelType!: string;
+  activeSource!: string;
+  activeTarget!: string;
+  activeLabel!: string;
+
+  ngOnInit(): void {
+    this.panelHidden = true;
+    this.activeSource = 'prestashop';
+    this.activeTarget = 'drupal';
   }
-  setCmsTarget(): string {
-    return 'drupal';
+
+  openPanel(origin: string) {
+    this.activeLabel =
+      origin === 'source' ? this.activeSource : this.activeTarget;
+    this.panelType = origin;
+    this.panelHidden = false;
+    this.setActiveLabel();
+  }
+  close() {
+    this.panelHidden = true;
+  }
+  setActiveLabel() {
+    const id = this.activeLabel + '-label';
+    const button = document.getElementById(id);
+    button?.classList.add('active-cms');
+  }
+
+  update(label: string) {
+    if (this.panelType === 'source') {
+      this.activeSource = label;
+    } else {
+      this.activeTarget = label;
+    }
+    this.activeLabel = label;
+    this.panelHidden = true;
   }
 }
