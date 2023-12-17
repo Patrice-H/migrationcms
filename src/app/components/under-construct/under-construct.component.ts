fdbb50295b68eacfee1679bf579e4caf6c4e0476
Patrice-H <webmestre64@gmail.com>
2023-12-17 11:12:07 +0100

    Adds component

---
+++ src/app/components/under-construct/under-construct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-under-construct',
+  templateUrl: './under-construct.component.html',
+  styleUrls: ['./under-construct.component.scss']
+})
+export class UnderConstructComponent {
+
+}
