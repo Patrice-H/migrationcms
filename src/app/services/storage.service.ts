c2dfcf2475879b93ca3b2ea3a0a284ad602c59a9
Patrice-H <webmestre64@gmail.com>
2023-12-17 11:14:16 +0100

    Updates service

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class StorageService {
   emptyStorage: boolean = this.getLocalStorage() === null ? true : false;
 
-  createLocalStorage() {
+  createLocalStorage(): void {
     const connexionDate = new Date();
     const expiresMonth = connexionDate.getMonth() + 1;
     const expiresDate = new Date(
@@ -25,20 +25,24 @@ export class StorageService {
       localStorage.setItem('user-preferences', JSON.stringify(userPref));
     }
   }
-  getLocalStorage() {
+  getLocalStorage(): any {
     return localStorage.getItem('user-preferences');
   }
-  updateLastConnexion(): void {
+  getStorage() {
+    this.emptyStorage ? this.createLocalStorage() : null;
     const rawData = this.getLocalStorage();
-    const now = new Date();
     let storage = rawData === null ? null : JSON.parse(rawData);
+
+    return storage;
+  }
+  updateLastConnexion(): void {
+    const now = new Date();
+    let storage = this.getStorage();
     storage.lastConnexion = now;
     localStorage.setItem('user-preferences', JSON.stringify(storage));
   }
   updateEmail(mail: string): boolean {
-    this.emptyStorage ? this.createLocalStorage() : null;
-    const rawData = this.getLocalStorage();
-    let storage = rawData === null ? null : JSON.parse(rawData);
+    let storage = this.getStorage();
     if (storage.eMail === mail) {
       return false;
     }
@@ -47,19 +51,26 @@ export class StorageService {
 
     return true;
   }
+  setEmail(mail: string): void {
+    let storage = this.getStorage();
+    storage.eMail = mail;
+    localStorage.setItem('user-preferences', JSON.stringify(storage));
+  }
   setExpiryDate(): void {
-    const rawData = this.getLocalStorage();
     const now = new Date();
     const delay = now.getDate() + 1;
     const expireDate = new Date(now.setDate(delay));
-    let storage = rawData === null ? null : JSON.parse(rawData);
+    let storage = this.getStorage();
     storage.expires = expireDate;
     localStorage.setItem('user-preferences', JSON.stringify(storage));
   }
-
+  setUserName(name: string): void {
+    let storage = this.getStorage();
+    storage.userName = name;
+    localStorage.setItem('user-preferences', JSON.stringify(storage));
+  }
   isConnected(): boolean {
-    const rawData = this.getLocalStorage();
-    let storage = rawData === null ? null : JSON.parse(rawData);
+    let storage = this.getStorage();
     const now = new Date();
     const expiresDate = storage === null ? now : new Date(storage.expires);
     if (expiresDate > now && storage.token !== '') {
@@ -68,16 +79,14 @@ export class StorageService {
 
     return false;
   }
-  writeToken() {
-    const rawData = this.getLocalStorage();
-    let storage = rawData === null ? null : JSON.parse(rawData);
+  writeToken(): void {
+    let storage = this.getStorage();
     storage.token =
       '6hrFDATxrG9w14QY9wwnmVhLE0Wg6LIvwOwUaxz761m1JfRp4rs8Mzozk5xhSkw0_MQz6bpcJnrFUDwp5lPPFC157dHxbkKlDiQ9XY3ZIP8zAGCsS8ruN2uKjIaIargX';
     localStorage.setItem('user-preferences', JSON.stringify(storage));
   }
-  disconnect() {
-    const rawData = this.getLocalStorage();
-    let storage = rawData === null ? null : JSON.parse(rawData);
+  disconnect(): void {
+    let storage = this.getStorage();
     storage.token = '';
     storage.expires = '';
     localStorage.setItem('user-preferences', JSON.stringify(storage));
