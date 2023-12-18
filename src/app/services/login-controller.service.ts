9118a73ff7c5b43418c4f93f9fe45d77f7e15e89
Patrice-H <webmestre64@gmail.com>
2023-12-18 09:45:13 +0100

    Updates service

--- src/app/services/login-controller.service.ts
+++ src/app/services/login-controller.service.ts
@@ -8,25 +8,40 @@ import { MenuService } from 'src/app/services/menu.service';
 export class LoginControllerService {
   constructor(private storage: StorageService, private menu: MenuService) {}
 
-  controlEmail(event: EventEmitter<string>) {
-    if (this.storage.isConnected()) {
-      /* control backend  */
-      /* email unknown =>
+  controlEmail(event: EventEmitter<string>, email: string): void {
+    if (email === 'a@aa.aaa') {
+      this.storage.setUserName('a a');
+      event.emit('logIn');
+    } else {
+      event.emit('signUp');
+    }
+    this.storage.setEmail(email);
+    //if (this.storage.isConnected()) {
+    /* control backend  */
+    /* email unknown =>
       this.nextStep.emit('signup');
-      /* email match => */
-      event.emit('account');
+      /* email match => 
+      event.emit('connect');
     } else {
       event.emit('logIn');
-    }
+    }*/
   }
 
-  controlPassword(event: EventEmitter<string>, errorMessage: string) {
+  controlPassword(
+    event: EventEmitter<string>,
+    password: string,
+    errorMessage: string
+  ): void {
+    if (password === 'aaaa') {
+      this.storage.writeToken();
+      event.emit('connect');
+    }
     /** control backend **/
     /* => pass unknown 
       errorMessage = this.formElement.invalidPasswordError;
       this.formElement.displayError('pass')
-      /* =>  pass match */
+      /* =>  pass match 
     this.storage.writeToken();
-    this.menu.navigate(event, 'account');
+    this.menu.navigate(event, 'account');*/
   }
 }
